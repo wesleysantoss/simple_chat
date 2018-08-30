c27c7153a4a751c61f552ef2b257f0b3c294aa09
Wesley Santos <wessleysanttos@live.com>
2018-08-29 21:27:15 -0300

    Baixado o modulo dotenv

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 'use strict';
 
-const mongoose = require('mongoose');
+require('dotenv').config();
+const mongoose = require('mongoose'),
+      URI      = process.env.MONGO_URI || 'mongodb://localhost:27017/simplechat';
 
-mongoose.connect('mongodb://localhost:27017/simplechat', { useNewUrlParser: true });
+mongoose.connect(URI, { useNewUrlParser: true });
+
 mongoose.connection.on('connected', () =>  console.log(`MongoDB connected`));
 mongoose.connection.on('error', err =>  console.log(`MongoDB error: ${err}`));
 
